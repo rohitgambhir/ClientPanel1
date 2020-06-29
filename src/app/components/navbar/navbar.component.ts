8039ad2c80e470fed145f8bdc22e3cf6af4f406a
ROHIT <rohitgambhir96@gmail.com>
2020-06-29 15:17:50 +0530

    login feature added.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthService} from '../../services/auth.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Client} from '../../models/Client';
+import{Router} from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean; // kya login hai 
+  loggedInUser: string;
+  // logged in konsa hai 
+  showRegister: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit(): void {
+    this.authService.getAuth().subscribe(auth=>{
+      if(auth){
+        this.loggedInUser=auth.email;
+        this.isLoggedIn = true;
+      } else {
+         this.isLoggedIn = false;
+      }
+   });
+  }
+  onLogoutClick(){
+     this.authService.logout();
+     this.flashMessage.show('You are now logged out' , {
+       cssClass: 'alert-success' , timeout:4000
+     })
   }
 
 }
